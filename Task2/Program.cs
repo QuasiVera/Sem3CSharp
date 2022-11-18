224628f661f108c15e8880ec50741a678a2054df
Phail Safiullin <safiullin.fail@yandex.ru>
2022-11-18 22:45:02 +0300

    Add Tasks

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int N = 0;
+
+InitQuarterByUser(ref N);
+PrintCoord(N);
+
+static void InitQuarterByUser(ref int N)
+{
+    Console.Write("Введите номер четверти от 1 до 4: ");
+    int N = int.Parse(Console.ReadLine() ?? "");
+}
+
+static void PrintCoord(int N)
+{
+    if (N == 1)
+        Console.WriteLine("Диапозон значений: Х > 0, Y > 0");
+
+    else if (N == 2)
+        Console.WriteLine("сдДиапозон значений: Х < 0, Y > 0");
+
+    else if (N == 3)
+        Console.WriteLine("Диапозон значений: Х < 0, Y < 0");
+
+    else if (N == 4)
+        Console.WriteLine("Диапозон значений: Х > 0, Y < 0");
+
+    else
+        Console.WriteLine("Ошибка ввода");
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+double X1 = UserInput("Введите координату X1: ");
+
+double Y1 = UserInput("Введите координату Y1: ");
+
+double X2 = UserInput("Введите координату X2: ");
+
+double Y2 = UserInput("Введите координату Y2: ");
+
+double res = DisPoint (X1, Y1, X2, Y2);
+Console.WriteLine($"{res}");
+
+static double DisPoint (double X1, double Y1, double X2, double Y2)
+{
+    return Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+}
+
+static double UserInput (string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine() ?? ""); 
+}
